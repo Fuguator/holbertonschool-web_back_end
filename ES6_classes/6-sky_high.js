cf508cb9ab2a270b1e5b09ed90dcfec774191ce0
Fuguator <abbsvfuad@gmail.com>
2024-11-14 16:57:21 +0400

    fix

--- ES6_classes/6-sky_high.js
+++ ES6_classes/6-sky_high.js
@@ -2,7 +2,7 @@
 /* eslint-disable-next-line import/extensions */
 import Building from './5-building.js';
 
-export default class SkyHighBuilding extends Building{
+export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
     this._floors = floors;
