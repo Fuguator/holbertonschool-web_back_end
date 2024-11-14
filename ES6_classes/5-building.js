4402e0d9c0f7277e582f500cb167d0f97d2e66e4
Fuguator <abbsvfuad@gmail.com>
2024-11-14 16:23:35 +0400

    fix

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,13 +1,14 @@
 export default class Building {
-  constructor(sqft) {
-    super(sqft);
-  }
+    constructor(sqft) {
+        if (this.constructor !== Building
+        && !this.evacuationWarningMessage) {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+        }
+        this._sqft = sqft;
+    }
 
   get sqft() {
     return this._sqft;
   }
 
-  evacuationWarningMessage() {
-    throw new Error("Class extending Building must override evacuationWarningMessage");
-  }
 }
