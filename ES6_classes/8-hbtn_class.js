293839081b3641f63964e27772615376763ad9c5
Fuguator <abbsvfuad@gmail.com>
2024-11-14 17:11:34 +0400

    add

---
+++ ES6_classes/8-hbtn_class.js
@@ -0,0 +1,16 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this._size = 'Number';
+    this._location = 'String';
+    
+    this._size = size;
+    this._location = location;
+  }
+
+  toString(){
+    return `${this._location}`;
+  }
+  valueOf() { 
+    return `${this._size}`;
+  }
+}
