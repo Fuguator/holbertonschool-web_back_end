75050ca43b52f931c38a82ff59b29f2bceef4027
Fuguator <abbsvfuad@gmail.com>
2024-11-14 15:42:38 +0400

    fix

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -3,6 +3,13 @@ export default class Currency {
     this._code = code;
     this._name = name;
   }
+  get name() {
+    return this._name;
+  }
+  get code() {
+    return this._code;
+  }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
