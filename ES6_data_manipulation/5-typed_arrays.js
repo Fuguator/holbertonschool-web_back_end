080f6d1966585b2e59b4bf47684a5d99b603ae80
Fuguator <abbsvfuad@gmail.com>
2024-11-19 19:33:17 +0400

    add

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -3,7 +3,7 @@ export default function createInt8TypedArray(length, position, value) {
   const ArrayBuffer = new Int8Array(length);
 
   if (position >= length || position < 0)
-    throw Error('Position outside range');
+    throw new Error('Position outside range');
 
   ArrayBuffer[position] = value;
   return ArrayBuffer;
