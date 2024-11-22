568dfbd2501124171428295b52f1dfdae2fdeaaf
Fuguator <abbsvfuad@gmail.com>
2024-11-22 18:47:22 +0400

    fix

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,10 @@
-/*eslint-disable */
 export default function createInt8TypedArray(length, position, value) {
-    const typedArray = new Int8Array(length);
-  
-    if (position >= length || position < 0) {
-      throw new Error('Position outside range');
-    }
-  
-    typedArray[position] = value;
-    return typedArray;
+  const arrBuff = new ArrayBuffer(length);
+  const int8view = new Int8Array(arrBuff);
+  if (position > length) {
+    throw Error('Position outside range');
+  } else {
+    int8view[position] = value;
   }
-  
+  return new DataView(arrBuff);
+}
