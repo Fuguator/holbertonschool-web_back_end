000ea41d4db6e41b34293e0d3dc3d55415f351cd
Fuguator <abbsvfuad@gmail.com>
2024-11-25 10:46:17 +0400

    add

---
+++ ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,7 @@
+/*eslint-disable */
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) throw new Error('Cannot process');
+  return map.forEach((value, key) => {
+    if (value === 1) map.set(key, 100);
+  });
+}
