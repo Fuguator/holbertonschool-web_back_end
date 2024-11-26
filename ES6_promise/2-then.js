965c02f7890948ee82f39bad551f77cc5a32215a
Fuguator <abbsvfuad@gmail.com>
2024-11-26 11:23:26 +0400

    fix

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,6 +1,6 @@
 /*eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  promise
+  return promise
   .then((res) => {
     console.log({status: 200, body: 'success'});
   })
