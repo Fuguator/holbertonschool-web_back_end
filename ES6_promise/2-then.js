743b5976c6f06c6a2c43e984e2fa35c05432ab41
Fuguator <abbsvfuad@gmail.com>
2024-11-26 11:37:28 +0400

    fix

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -1,13 +1,12 @@
 /*eslint-disable */
 export default function handleResponseFromAPI(promise) {
-  return promise
-  .then((res) => {
-    console.log({status: 200, body: 'success'});
-  })
-  .catch((error) => {
-    console.log(error);
-  })
-  .finally(() => {
-    console.log('Got a response from the API');
-  })
+    return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return { status: 200, body: 'success' };
+    })
+    .catch(() => {
+      console.log('Got a response from the API');
+      return new Error();
+    });
 }
