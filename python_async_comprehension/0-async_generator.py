8d5f97d2da8b1ef170c50c497198b8f021abf5ae
Fuguator <abbsvfuad@gmail.com>
2024-12-09 16:34:29 +0400

    fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,14 +2,8 @@
 """Async Generator"""
 import asyncio
 import random
-
-
 async def async_generator():
     """Async Generator"""
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
-    
-async def print_yielded_values():
-    async for value in async_generator():
-        print(value)
