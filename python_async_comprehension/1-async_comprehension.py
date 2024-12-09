b0def4a19bb84cb90355157cee31be1f803fe130
Fuguator <abbsvfuad@gmail.com>
2024-12-09 16:51:41 +0400

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """Async Comprehensions"""
-import typing
+from typing import Generator
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> typing.Generator[float, None, None]:
+async def async_comprehension() -> Generator[float, None, None]:
     """Async Comprehensions"""
     return [i async for i in async_generator()]
